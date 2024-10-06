73307ddbdadc5df0a7d07ac538ec88619a24a24d
kk7188048 <kk7188048@g]mail.com>
2024-10-06 15:30:50 +0530

    Improving

--- audio-processor.js
+++ audio-processor.js
@@ -1,16 +1,54 @@
-class AudioProcessor extends AudioWorkletProcessor {
-    process(inputs, outputs, parameters) {
-      const input = inputs[0];  // The first input (microphone)
-      const inputChannel = input[0];  // The first channel
-  
-      if (inputChannel) {
-        // Send audio data back to the main thread (for WebSocket)
-        this.port.postMessage(inputChannel);
-      }
-  
-      return true;  // Keep processing
+const io = require('socket.io-client');
+
+let socket = io("http://localhost:3000", {
+    transports: ['websocket'],
+    upgrade: false
+});
+
+socket.on('connect', () => {
+  navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+    .then((stream) => {
+        var madiaRecorder = new MediaRecorder(stream);
+        var audioChunks = [];
+
+        madiaRecorder.addEventListener("dataavailable", function (event) {
+            audioChunks.push(event.data);
+        });
+
+        madiaRecorder.addEventListener("stop", function () {
+            var audioBlob = new Blob(audioChunks);
+            audioChunks = [];
+            var fileReader = new FileReader();
+            fileReader.readAsDataURL(audioBlob);
+            fileReader.onloadend = function () {
+                var base64String = fileReader.result;
+                socket.emit("audioStream", base64String);
+            };
+
+            madiaRecorder.start();
+            setTimeout(function () {
+                madiaRecorder.stop();
+            }, 1000);
+        });
+
+        madiaRecorder.start();
+        setTimeout(function () {
+            madiaRecorder.stop();
+        }, 1000);
+    })
+    .catch((error) => {
+        console.error('Error capturing audio.', error);
+    });
+});
+
+socket.on('audioStream', (audioData) => {
+    var newData = audioData.split(";");
+    newData[0] = "data:audio/ogg;";
+    newData = newData[0] + newData[1];
+
+    var audio = new Audio(newData);
+    if (!audio || document.hidden) {
+        return;
     }
-  }
-  
-  registerProcessor('audio-processor', AudioProcessor);
-  
+    audio.play();
+});
